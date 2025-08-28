b81dade0ae362281d103a8ed1250eb8f14db8df2
jlaw1-rgb <jlaw1@hwemail.com>
2025-08-28 14:55:16 -0700

    Worked Aug 28

---
+++ Day 0/Door.java
@@ -0,0 +1,44 @@
+public class Door {
+    //can only open one way
+    //has a lock on one side
+    //can be locked or unlocked
+    //does not open from outside if closed
+    //rectangular shaped
+    
+    private int numberOfLocks; // = 1; 
+    private boolean canBeLocked; // = true;
+    private String shape; // = "rectangular";
+    private double height; // = 7.0;
+    private char name; // = "D";
+
+    public Door() {
+        numberOfLocks = 1;
+        canBeLocked = true;
+        shape = "rectangular";
+        height = 7.0;
+        name = 'D';
+    }
+
+    // public WhatComesOut nameOfFunction(input) {
+
+    public void numberOfLocks() {
+        numberOfLocks = 0
+        System.out.println(numberOfLocks);
+    }
+
+    public void canBeLocked() {
+        canBeLocked = false;
+    }
+
+    public void shape() {
+        shape = "square";
+    }
+
+    public void height() {
+        height = 6.8;
+    }
+
+    public void name() {
+        name = "Miao"
+    }
+}
---
+++ Day 0/DoorTester.java
@@ -0,0 +1,5 @@
+public class DoorTester {
+    public static void main (String[] args){
+
+    }
+}
