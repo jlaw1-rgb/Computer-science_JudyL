b2b68c3b2fc77d96544a6888a9b758bb56f7e54c
jlaw1-rgb <jlaw1@hwemail.com>
2025-10-21 13:38:09 -0700

    oct 21 after

---
+++ Unit1/TriangleLoops.java
@@ -0,0 +1,104 @@
+public class TriangleLoops {
+
+    /*
+     * The method returns a String by creating rows of letters. Each row prints a
+     * number of letters equal to the row number. The total number of rows printed
+     * is determined by numberOfRows.
+     * @param numberOfRows the number of rows in the triangle
+     * @param letter the letter to be printed
+     * @return a String of letters in the shape of a triangle
+     */
+    // to-do: implement createLetterTriangleUp
+    public static String createLetterTriangleUp(int numberOfRows, char letter) {
+        String triangle = "";
+        for (int i = 0; i <= numberOfRows; i++) {
+            for (int n = 0; n < i; n++) {
+                triangle = triangle + letter;
+            }
+            triangle = triangle + "\n";
+        }
+        return triangle;
+    }
+
+    /*
+     * The method returns a String by creating rows of letters. Each row prints a
+     * number of letters equal to the total number of rows, descending downward. The
+     * total number of rows printed is determined by numberOfRows.
+     * @param numberOfRows the number of rows in the triangle
+     * @param letter the letter to be printed
+     * @return a String of letters in the shape of a triangle
+     */
+    // to-do: implement createLetterTriangleDown
+    public static String createLetterTriangleDown(int numberOfRows, char letter) {
+        String triangle = "";
+        for (int i = numberOfRows; i >= 0; i = i - 1) {
+            for (int n = i; n > 0; n = n - 1) {
+                triangle = triangle + letter;
+            }
+            triangle = triangle + "\n";
+        }
+        return triangle;
+    }
+
+    /*
+     * This method returns a String of a number triangle. The number of rows in the
+     * triangle is determined by the numberOfRows, and the number used in each row
+     * is determined by the row number.
+     * @param numberOfRows the number of rows in the triangle
+     * @return a String of numbers in the shape of a triangle
+     */
+    // to-do: implement createNumbersTriangle
+    public static String createNumbersTriangle(int numberOfRows) {
+        String triangle = "";
+        for (int i = 0; i <= numberOfRows; i++) {
+            for (int n = 0; n < i; n++) {
+                triangle = triangle + i + " ";
+            }
+            triangle = triangle + "\n";
+        }
+        return triangle;
+    }
+
+    /*
+     * The method returns a String by creating a pyramid pattern using the alphabet.
+     * The method determines the height of the pyramid by using the int
+     * numberOfRows.
+     * @param numberOfRows the number of rows in the triangle
+     * @return a String of letters in the shape of a pyramid
+     */
+    // to-do: implement createAlphabetTriangle
+    public static String createAlphabetTriangle(int numberOfRows) {
+        if (numberOfRows > 26) {
+            numberOfRows = 26;
+        }
+        String triangle = "";
+        for () {
+            for (int i = numberOfRows; i >= 0; i = i - 1) {
+                for (int n = i; n > 0; n = n - 1) {
+                    triangle = triangle + letter;
+                }
+                triangle = triangle + "\n";
+            }
+            for () {
+                // descending letters
+            }
+            for () {
+                // ascending letters
+            }
+        }
+        return triangle;
+    }
+
+
+    public static String reference(int numberOfRows, char letter) {
+        String triangle = "";
+        for (int i = numberOfRows; i >= 0; i = i - 1) {
+            for (int n = i; n > 0; n = n - 1) {
+                triangle = triangle + letter;
+            }
+            triangle = triangle + "\n";
+        }
+        return triangle;
+    }
+
+}
---
+++ Unit1/TriangleLoopsTester.java
@@ -0,0 +1,23 @@
+public class TriangleLoopsTester {
+    public static void main(String[] args) {
+        // to-do: create a separate tester class
+        // to-do: invoke each method at least 2 times in the tester class
+
+        // for example:
+        // System.out.println(createLetterTriangleUp(3, 'A'));
+        // System.out.println(createLetterTriangleUp(15, 'B'));
+
+        String output = TriangleLoops.createLetterTriangleUp(3, 'A');
+        System.out.println(output);
+        String output2 = TriangleLoops.createLetterTriangleUp(5, 'B');
+        System.out.println(output2);
+
+        System.out.println(TriangleLoops.createLetterTriangleDown(3, 'A'));
+        System.out.println(TriangleLoops.createLetterTriangleDown(5, 'B'));
+
+        System.out.println(TriangleLoops.createNumbersTriangle(4));
+
+        System.out.println(TriangleLoops.createAlphabetTriangle(5));
+    }
+
+}
