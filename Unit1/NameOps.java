48eafc243840e1961a4d9e2a42d4fbee40cb7813
jlaw1-rgb <jlaw1@hwemail.com>
2025-10-21 10:41:23 -0700

    oct 21 before

---
+++ Unit1/NameOps.java
@@ -0,0 +1,15 @@
+public class NameOps {
+
+    public static String printMethodCall(String method, String name) {
+        return "" + method + "(\"" + name + "\"): ";
+    }
+
+    public static String whoIsAwsome(String name) {
+        return "" + name + "is awsome! ";
+    }
+
+    public static int indexOfFirstSpace(String name) {
+        return 
+    }
+
+}
